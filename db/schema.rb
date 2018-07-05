992c25d629fb36e999913216c09b6ab69b0adb96
Muhammad Mohsin <mohsin2015444@gmail.com>
2018-07-05 14:06:30 +0500

    Users Model and table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_082040) do
+ActiveRecord::Schema.define(version: 2018_07_05_084757) do
 
   create_table "animals", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2018_06_27_082040) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "serialno"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
